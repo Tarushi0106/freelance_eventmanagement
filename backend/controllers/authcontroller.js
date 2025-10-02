6da3aee4ab6efbd1d1564fbd4b542900a07fc2a4
Tarushi Chaudhary <tarushi0106@gmail.com>
2025-10-02 18:16:12 +0530

    Initial commit

---
+++ backend/controllers/authcontroller.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+// REGISTER controller
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+          const qrData = `${newUser._id}`;
+    const qrCode = await QRCode.toDataURL(qrData);
+
+    
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Error registering user" });
+    }
+};
+
+module.exports = { registerUser };
